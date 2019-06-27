7c516be2558ffd2da2834f92b70dbe513cd53beb
sarrasoussia <52163754+sarrasoussia@users.noreply.github.com>
2019-06-27 12:25:13 +0200

    Add files via upload

---
+++ gmc/hey you/index.js
@@ -0,0 +1,11 @@
+var textarea=document.getElementById("text");
+
+function changecolor() {
+
+    if (textarea.style.color=="white") {
+        textarea.style.color="pink"
+    }
+    else {
+        textarea.style.color="white";
+    }
+
